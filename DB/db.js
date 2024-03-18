ec8e3963afc6f56159b864d8f24dd935b993a826
AsifAhmedAli <asif@mexil.it>
2024-03-19 03:25:50 +0500

    db error

--- DB/db.js
+++ DB/db.js
@@ -16,21 +16,35 @@
 
 // module.exports = db;
 
-const mysql = require("mysql");
-require("dotenv").config();
+// const mysql = require("mysql");
+// require("dotenv").config();
 
 // Create a connection for the database
-const db = mysql.createConnection({
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.pass,
-  database: process.env.dbname,
-});
+// const db = mysql.createConnection({
+//   host: process.env.host,
+//   user: process.env.user,
+//   password: process.env.pass,
+//   database: process.env.dbname,
+// });
 
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected to authentic crafts DB!");
-});
+// db.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected to authentic crafts DB!");
+// });
+
+
+const mysql = require("mysql2");
+require("dotenv").config();
+// const connection = () => {
+  const db = mysql.createPool({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.pass,
+    database: process.env.dbname,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
 
 // Create a connection for the omni-channel database
 // const omniChannelDB = mysql.createConnection({
